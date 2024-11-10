1b27b77001e69b1b0f2e521dc16cb228548adb4e
Lucas Mørch <lucasmoerch04@icloud.com>
2024-11-10 18:48:33 +0100

    build app

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vuetify from 'vite-plugin-vuetify' // Import Vuetify plugin
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vuetify()],
+  base: '/KanbanBoard/',
   server: {
     watch: {
       usePolling: true,
